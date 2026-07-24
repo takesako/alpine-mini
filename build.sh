76cf9d006889acc17836a198d12488ad0ae0116f
Yoshinori Takesako <takesako@gmail.com>
2026-07-25 02:00:44 +0900

    full busybox commands, apk add --initdb

--- build.sh
+++ build.sh
@@ -24,19 +24,56 @@ cp "$T/busybox-static/bin/busybox.static" "$F/bin/busybox"
 cp "$T/apk-tools-static/sbin/apk.static" "$F/sbin/apk"
 chmod 755 "$F/bin/busybox" "$F/sbin/apk"
 
-for x in sh test true false printf cat head tail echo touch cp mv rm mkdir rmdir \
-  ln ls stat find grep sed awk cut sort uniq wc xargs chmod chown date uptime \
-  uname hostname sleep tar cpio gzip gunzip dd sync ps free df du kill dmesg \
-  vi ip ping nslookup wget udhcpc mount umount switch_root chroot setsid blkid \
-  mkswap swapon swapoff lsmod insmod rmmod reboot poweroff
-do ln -sf /bin/busybox "$F/bin/$x"; done
+for x in \
+  '[' '[[' acpid add-shell addgroup adduser adjtimex arch arp arping ash awk \
+  base64 basename bbconfig bc beep blkdiscard blkid blockdev brctl bunzip2 \
+  bzcat bzip2 cal cat chattr chgrp chmod chown chpasswd chroot chvt cksum \
+  clear cmp comm cp cpio crond crontab cryptpw cut date dc dd deallocvt \
+  delgroup deluser depmod df diff dirname dmesg dnsdomainname dos2unix du \
+  dumpkmap echo egrep eject env ether-wake expand expr factor fallocate \
+  false fatattr fbset fbsplash fdflush fdisk fgrep find findfs flock fold \
+  free fsck fstrim fsync fuser getopt getty grep groups gunzip gzip halt hd \
+  head hexdump hostid hostname hwclock id ifconfig ifdown ifenslave ifup init \
+  inotifyd insmod install ionice iostat ip ipaddr ipcalc ipcrm ipcs iplink \
+  ipneigh iproute iprule iptunnel kbd_mode kill killall killall5 klogd last \
+  less link linux32 linux64 ln loadfont loadkmap logger login logread losetup \
+  ls lsattr lsmod lsof lsusb lzcat lzma lzop lzopcat makemime md5sum mdev \
+  mesg microcom mkdir mkdosfs mkfifo mkfs.vfat mknod mkpasswd mkswap mktemp \
+  modinfo modprobe more mount mountpoint mpstat mv nameif nanddump nandwrite \
+  nbd-client nc netstat nice nl nmeter nohup nologin nproc nsenter nslookup \
+  ntpd od openvt partprobe passwd paste pgrep pidof ping ping6 pipe_progress \
+  pivot_root pkill pmap poweroff printenv printf ps pscan pstree pwd pwdx \
+  raidautorun rdate rdev readahead readlink realpath reboot reformime \
+  remove-shell renice reset resize rev rfkill rm rmdir rmmod route run-parts \
+  sed sendmail seq setconsole setfont setkeycodes setlogcons setpriv setserial \
+  setsid sh sha1sum sha256sum sha3sum sha512sum showkey shred shuf slattach \
+  sleep sort split ssl_client stat strings stty su sum swapoff swapon \
+  switch_root sync sysctl syslogd tac tail tar tee test time timeout top touch \
+  tr traceroute traceroute6 tree true truncate tty ttysize tunctl udhcpc \
+  udhcpc6 umount uname unexpand uniq unix2dos unlink unlzma unlzop unshare \
+  unxz unzip uptime usleep uudecode uuencode vconfig vi vlock volname watch \
+  watchdog wc wget which who whoami whois xargs xxd xzcat yes zcat zcip
+do
+  ln -sf /bin/busybox "$F/bin/$x"
+done
 
-for x in mount umount switch_root chroot udhcpc blkid mkswap swapon swapoff \
-  lsmod insmod rmmod reboot poweroff
-do ln -sf /bin/busybox "$F/sbin/$x"; done
+for x in \
+  acpid add-shell addgroup adduser adjtimex arp arping blkdiscard blkid \
+  blockdev brctl chpasswd chroot crond cryptpw deallocvt delgroup deluser \
+  depmod fdflush fdisk findfs fsck fstrim getty halt hwclock ifconfig ifdown \
+  ifenslave ifup insmod ip klogd loadfont loadkmap logread losetup lsmod \
+  mdev mkdosfs mkfs.vfat mknod mkpasswd mkswap modinfo modprobe mount \
+  nameif nanddump nandwrite nbd-client partprobe pivot_root poweroff rdate \
+  rdev readahead reboot remove-shell rfkill rmmod route setconsole setfont \
+  setkeycodes setlogcons setserial slattach swapoff swapon switch_root sysctl \
+  syslogd tunctl udhcpc udhcpc6 umount vconfig watchdog zcip
+do
+  ln -sf /bin/busybox "$F/sbin/$x"
+done
 
 cp "$T/alpine-keys/etc/apk/keys/"* "$F/etc/apk/keys/"
 cp "$T/ca-certificates-bundle/etc/ssl/certs/ca-certificates.crt" "$F/etc/ssl/certs/"
+ln -sf certs/ca-certificates.crt "$F/etc/ssl/cert.pem"
 
 L=$T/linux-virt
 K=$(basename "$(find "$L/lib/modules" -mindepth 1 -maxdepth 1 -type d)")
@@ -106,15 +143,15 @@ mount -t devtmpfs devtmpfs /dev 2>/dev/null || true
 while read -r m; do insmod "$m" || exec sh </dev/console >/dev/console 2>&1; done </etc/modules
 mount -t ext4 -o noatime,nodiratime /dev/vda /newroot ||
   exec sh </dev/console >/dev/console 2>&1
-[ -e /newroot/sbin/init ] || cp -a /bin /sbin /usr /etc /lib /root /var /newroot/
+[ -e /newroot/sbin/boot ] || cp -a /bin /sbin /usr /etc /lib /root /var /newroot/
 for d in proc sys dev tmp run mnt/share; do mkdir -p "/newroot/$d"; done
 mount --move /proc /newroot/proc
 mount --move /sys /newroot/sys
 mount --move /dev /newroot/dev
-exec switch_root /newroot /sbin/init
+exec switch_root /newroot /sbin/boot
 EOF
 
-cat >"$F/sbin/init" <<'EOF'
+cat >"$F/sbin/boot" <<'EOF'
 #!/bin/sh
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin
 hostname alpine
@@ -133,6 +170,6 @@ fi
 exec setsid sh -c 'exec sh </dev/ttyS0 >/dev/ttyS0 2>&1'
 EOF
 
-chmod 755 "$F/init" "$F/sbin/init" "$F/usr/share/udhcpc/default.script"
-(cd "$F"; find . | cpio -o -H newc 2>/dev/null | gzip -9) >initramfs.cpio.gz
+chmod 755 "$F/init" "$F/sbin/boot" "$F/usr/share/udhcpc/default.script"
+(cd "$F"; find . | cpio -o -H newc -R 0:0 2>/dev/null | gzip -9) >initramfs.cpio.gz
 ls -lh vmlinuz-virt initramfs.cpio.gz
